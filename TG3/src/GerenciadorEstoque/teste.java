97d9182f9d13b206470b9855d775ba01d5f3dbc4
Manoela Resende <manoresende@outlook.com>
2023-12-10 23:06:06 -0300

    fiscal ops working

--- TG3/src/GerenciadorEstoque/teste.java
+++ TG3/src/GerenciadorEstoque/teste.java
@@ -9,15 +9,15 @@ public class teste {
         p.setValidade("10-04-2024"); //tratamento de excecoes
         System.out.println(p);*/
 
-/*        ProdutoUN un = new ProdutoUN("camisa", "teste", 60.00, 1);
-        System.out.println(un);
+  /*     ProdutoUN un = new ProdutoUN("camisa", "teste", 60.00, 1);
+        System.out.println(un);*/
 
-        ProdutoKG kg = new ProdutoKG("cafe", "forte", 15.50, 0.8);
-        System.out.println(kg);
-        ProdutoKG kg2 = new ProdutoKG("chocolate", "branco", 7.50, 0.1);
-        System.out.println(kg);
+        ProdutoKG kg = new ProdutoKG("cafe", "forte", 15.50, 1);
+        //System.out.println(kg);
+        ProdutoKG kg2 = new ProdutoKG("queijo", "mussarela", 32.0, 1);
+        //System.out.println(kg);
 
-        // Cria um estoque
+/*        // Cria um estoque
         Estoque estoque = new Estoque();
 
         // Adiciona os produtos ao estoque
@@ -36,7 +36,7 @@ public class teste {
         // Remove um produto
         estoque.removeProduto(kg.getCodigo());
         System.out.println("Produto KG após remoção: " + estoque.getProduto(kg.getCodigo()));
-    */
+   */
         // Criação de alguns produtos
         Produto p1 = new ProdutoKG("Carne Moida", "Carne Moida 1", 20.0, 1);
         Produto p2 = new ProdutoUN("Leite", "Caixa com 1L", 3.50, 1);
@@ -48,6 +48,18 @@ public class teste {
         nf.adicionarItem(p1, 0.7); // 700g carne
         nf.adicionarItem(p2, 3); // 3 caixas
 
+        NotaFiscal nf2 = new NotaFiscal();
+        nf2.adicionarItem(kg,1.2);
+        nf2.adicionarItem(kg2,0.3);
+
+        GerenciadorNF gerenciadorNF = new GerenciadorNF();
+        gerenciadorNF.addNotaFiscal(nf);
+        gerenciadorNF.addNotaFiscal(nf2);
+        System.out.println(gerenciadorNF);
+
+        System.out.println("******************************");
+        gerenciadorNF.removeNotaFiscal(nf.getCodigo());
+        System.out.println(gerenciadorNF);
 // Imprimindo a nota fiscal
         System.out.println(nf);
 
