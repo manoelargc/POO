9006c71af0c02eb5c926f1485299f5650f48476f
Manoela Resende <manoresende@outlook.com>
2023-12-10 23:05:44 -0300

    create and implements fiscal manager

---
+++ TG3/src/GerenciadorEstoque/GerenciadorNF.java
@@ -0,0 +1,72 @@
+package GerenciadorEstoque;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorNF implements INotasFiscais {
+    private List<NotaFiscal> notasFiscais;
+
+    public GerenciadorNF() {
+        this.notasFiscais = new ArrayList<>();
+    }
+
+    @Override
+    public boolean addNotaFiscal(NotaFiscal nf) {
+        return notasFiscais.add(nf);
+    }
+
+    @Override
+    public boolean removeNotaFiscal(int codigo) {
+        return notasFiscais.removeIf(nf -> nf.getCodigo() == codigo);
+    }
+
+    @Override
+    public NotaFiscal getNotaFiscal(int codigo) {
+        for (NotaFiscal nf : notasFiscais) {
+            if (nf.getCodigo() == codigo) {
+                return nf;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean updateNotaFiscal(int codigo, NotaFiscal nova) {
+        for (int i = 0; i < notasFiscais.size(); i++) {
+            if (notasFiscais.get(i).getCodigo() == codigo) {
+                notasFiscais.set(i, nova);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addItem(int codigo, Item item) {
+        NotaFiscal nf = getNotaFiscal(codigo);
+        if (nf != null) {
+            nf.adicionarItem(item.getProduto(), item.getQuantidade());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(int codigo, Item item) {
+        NotaFiscal nf = getNotaFiscal(codigo);
+        if (nf != null) {
+            return nf.getItens().remove(item);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (NotaFiscal nf : notasFiscais) {
+            sb.append(nf.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+
+}
--- TG3/src/GerenciadorEstoque/INotasFiscais.java
+++ TG3/src/GerenciadorEstoque/INotasFiscais.java
@@ -1,54 +1,54 @@
-//package GerenciadorEstoque;
-//
-///**
-// * Interface que especifica os métodos míninos para manipular o conunto de notas fiscais.
-// * @author Aline
-// */
-//public interface INotasFiscais {
-//
-//    /**
-//     * Adiciona uma nota fiscal.
-//     * @param nf Nota fiscal a ser adicionada.
-//     * @return True se a nota for incluída com sucesso e False caso contrário.
-//     */
-//    public abstract boolean addNotaFiscal(NotaFiscal nf);
-//
-//    /**
-//     * Remove a nota fiscal com código informado.
-//     * @param codigo Código da nota fiscal a ser removida.
-//     * @return True se a nota for removida com sucesso e False caso contrário.
-//     */
-//    public abstract boolean removeNotaFiscal(int codigo);
-//
-//    /**
-//     * Captura a nota fiscal com a nota fiscal informada.
-//     * @param codigo Código da nota fiscal a ser capturada.
-//     * @return A nota fiscal com o código informado ou NULL caso a nota fiscal não seja encontrada.
-//     */
-//    public abstract NotaFiscal getNotaFiscal(int codigo);
-//
-//    /**
-//     * Altera a nota fiscal com o código informado.
-//     * @param codigo Código da nota fiscal a ser alterada.
-//     * @param nova Nota fiscal com as novas informações.
-//     * @return True se a nota fiscal foi corretamente atualizada e False caso contrário.
-//     */
-//    public abstract boolean updateNotaFiscal(int codigo, NotaFiscal nova);
-//
-//
-//    /**
-//     * Adiciona um item a nota fiscal com o código informado.
-//     * @param codigo Código da nota fiscal na qual o item deve ser adicionado.
-//     * @param item Item a ser adicionado.
-//     * @return True caso o item foi adicionado com sucesso e False caso a nota fiscal não exista ou a quantidade do produto em estoque seja menos do que a solicitada.
-//     */
-//    public boolean addItem(int codigo, Item item);
-//
-//    /**
-//     * Remove um item da nota fiscal com o código informado.
-//     * @param codigo Código da nota fiscal na qual o item deve ser removido.
-//     * @param item Item a ser removido.
-//     * @return True caso o item foi removido com sucesso e False caso contrário.
-//     */
-//    public boolean removeItem(int codigo, Item item);
-//}
+package GerenciadorEstoque;
+
+/**
+ * Interface que especifica os métodos míninos para manipular o conunto de notas fiscais.
+ * @author Aline Mello
+ */
+public interface INotasFiscais {
+
+    /**
+     * Adiciona uma nota fiscal.
+     * @param nf Nota fiscal a ser adicionada.
+     * @return True se a nota for incluída com sucesso e False caso contrário.
+     */
+    public abstract boolean addNotaFiscal(NotaFiscal nf);
+
+    /**
+     * Remove a nota fiscal com código informado.
+     * @param codigo Código da nota fiscal a ser removida.
+     * @return True se a nota for removida com sucesso e False caso contrário.
+     */
+    public abstract boolean removeNotaFiscal(int codigo);
+
+    /**
+     * Captura a nota fiscal com a nota fiscal informada.
+     * @param codigo Código da nota fiscal a ser capturada.
+     * @return A nota fiscal com o código informado ou NULL caso a nota fiscal não seja encontrada.
+     */
+    public abstract NotaFiscal getNotaFiscal(int codigo);
+
+    /**
+     * Altera a nota fiscal com o código informado.
+     * @param codigo Código da nota fiscal a ser alterada.
+     * @param nova Nota fiscal com as novas informações.
+     * @return True se a nota fiscal foi corretamente atualizada e False caso contrário.
+     */
+    public abstract boolean updateNotaFiscal(int codigo, NotaFiscal nova);
+
+
+    /**
+     * Adiciona um item a nota fiscal com o código informado.
+     * @param codigo Código da nota fiscal na qual o item deve ser adicionado.
+     * @param item Item a ser adicionado.
+     * @return True caso o item foi adicionado com sucesso e False caso a nota fiscal não exista ou a quantidade do produto em estoque seja menos do que a solicitada.
+     */
+    public boolean addItem(int codigo, Item item);
+
+    /**
+     * Remove um item da nota fiscal com o código informado.
+     * @param codigo Código da nota fiscal na qual o item deve ser removido.
+     * @param item Item a ser removido.
+     * @return True caso o item foi removido com sucesso e False caso contrário.
+     */
+    public boolean removeItem(int codigo, Item item);
+}
