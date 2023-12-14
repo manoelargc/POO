8efc220511ea42cf3466a4045d935cc469f4621d
Manoela Resende <manoresende@outlook.com>
2023-12-13 22:47:06 -0300

    load/store working

--- TG3/src/GerenciadorEstoque/Arquivo.java
+++ TG3/src/GerenciadorEstoque/Arquivo.java
@@ -1,36 +1,80 @@
 package GerenciadorEstoque;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Arquivo {
-    public static void gravar(Object obj, String nomeArquivo) {
-        try {
-            FileOutputStream fileOut = new FileOutputStream(nomeArquivo);
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(obj);
-            out.close();
-            fileOut.close();
-        } catch (IOException i) {
-            i.printStackTrace();
+    private static final String ESTOQUE_FILE = "estoque.txt";
+    private static final String GERENCIADOR_NF_FILE = "gerenciadorNF.txt";
+    private Estoque estoque;
+    private GerenciadorNF gerenciadorNF;
+
+    public Arquivo() {
+        this.estoque = carregarEstoque();
+        this.gerenciadorNF = carregarGerenciadorNF();
+    }
+
+    public Estoque getEstoque() {
+        return estoque;
+    }
+
+    public GerenciadorNF getGerenciadorNF() {
+        return gerenciadorNF;
+    }
+
+    public void salvarEstoque(Estoque estoque) {
+        try (PrintWriter writer = new PrintWriter(ESTOQUE_FILE)) {
+            for (Produto produto : estoque.getProdutos()) {
+                writer.println(produto.toCSV());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
-    public static Object ler(String nomeArquivo) {
-        Object obj = null;
-        try {
-            FileInputStream fileIn = new FileInputStream(nomeArquivo);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            obj = in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-            return null;
-        } catch (ClassNotFoundException c) {
-            System.out.println("Classe não encontrada");
-            c.printStackTrace();
-            return null;
+    public Estoque carregarEstoque() {
+        Estoque estoque = new Estoque();
+        File file = new File(ESTOQUE_FILE);
+        if (file.exists()) {
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    Produto produto = Produto.fromCSV(line);
+                    estoque.addProduto(produto);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
-        return obj;
+        return estoque;
+    }
+
+    public void salvarGerenciadorNF(GerenciadorNF gerenciadorNF) {
+        try (PrintWriter writer = new PrintWriter(GERENCIADOR_NF_FILE)) {
+            for (NotaFiscal nf : gerenciadorNF.getNotasFiscais()) {
+                writer.println(nf.toCSV());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public GerenciadorNF carregarGerenciadorNF() {
+        GerenciadorNF gerenciadorNF = new GerenciadorNF();
+        File file = new File(GERENCIADOR_NF_FILE);
+        if (file.exists()) {
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    NotaFiscal nf = NotaFiscal.fromCSV(line);
+                    gerenciadorNF.addNotaFiscal(nf);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return gerenciadorNF;
     }
 }
